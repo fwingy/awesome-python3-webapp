b1d66deccfba463d5eff7e77fddcf36b8a1a63b6
leonard12315 <lzzi386@outlook.com>
2019-03-26 22:57:52 +0800

    更新注释

--- www/orm.py
+++ www/orm.py
@@ -15,7 +15,7 @@ def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
     __pool = yield from aiomysql.create_pool(
-        host=kw.get('host', 'localhost'),
+        host=kw.get('host', 'localhost'), # dict的get方法
         port=kw.get('port', 3306),
         user=kw['user'],
         password=kw['password'],
